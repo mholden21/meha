e51f0426e59f651a28befeb598fd8e15a59360f6
Meha <meha_holden@berkeley.edu>
2025-09-17 13:02:28 -0700

    done with hw2

---
+++ homework2/homework2.py
@@ -0,0 +1,69 @@
+# File: homework2.py
+
+# Your file path should look like:
+# python_decal_fa25/yourname/homework2/homework2.py
+
+# Questions (Answer these in the homework2.py file as comments):
+
+# 1) What’s the difference between Git, GitHub, and Git Bash?
+#git bash is the terminal we use to run git, git hub is like an online "cloud " that stores code, and git is the version control system we use to navigate our folders/directories
+
+# 2) What’s the difference between the terminal and the command line?
+#terminal is the software interface that can be used to run typed command lines to navigate files and directories
+
+# 3) How does Windows PowerShell differ from Git Bash?
+#Windows powershell uses a different language than Git Bash, which is more similar to Apple's unix language.
+
+# 4) What’s the difference between Anaconda, conda, and Python?
+#python is the fundamental language that Anaconda and conda operate on. Conda is a management system, while anaconda is python tailored for data science and machine learning.
+
+# 5) What is VS Code?
+#VS Code is a website we use to run and edit python scripts to help debug.
+
+# 6) What is a Jupyter Notebook? How is it different from Jupyter Lab?
+#A Jupiter notebook is used to create and share documents of code. Jupyter notebook is a feature of Jupyter lab, which is a web-based user interface for Python.
+
+# 7) What does ~/ mean?
+# The ~ symbol means you're at the "home" or starting point of your command line, otherwise known as your home directory.
+
+# 8) What’s the difference between an absolute path and a relative path?
+#Absolute paths show location from the home directory, while relative paths show location based on the file/folder you're currently in.
+
+# 9) Imagine you're in your "yourname" repo. Write the absolute and relative paths to "course_assignments/homework2".
+#Absolte: ~/python_decal_fa25/course_assignments/homework2.py  Relative: ../course_assignments/homework2.py
+
+# 10) What command lets you move from "course_assignments/homework2/" to "course_assignments/"?
+#cd ..
+
+# 11) What would rm ./ do in your current directory? (Don’t try it!)
+#it would delete the current directory or file
+
+# 12) What do the following commands do?
+# git add = git add puts the file or directory in a staging area where it prepares the file to be saved
+# git commit = git commit saves the contents of the file to the local repository
+# git push = git push pushes the saved contents of the file to a remote repository
+
+# 13) What's the difference between "git add ." and "git add <file>"?
+#git add puts all changes in the staging area while git add <file> only places the changes made to the specified file into the staging area
+
+# 14) What do "git status" and "git log -1" do?
+#git status displays the status of your changes, whether saved or unsaved, as well as the name of your current branch. Git log-1 displays the most recent commit in the branch's history.
+
+# 15) What’s the difference between cloning a repository and pulling from it?
+#pulling a respository brings it from the remote repository to your local repository. Cloning a repository copies it onto your local machine, but doens't allow you to make remote changes to it.
+
+# 16) What has been your most frustrating bug or error in this class so far? How did you troubleshoot or fix it?
+#My most frustrating bug was when I was trying to connect my local and remote repositories through github. There were multiple errors, including the fact that the repositories didn't match, but I went to office hours and learned how to pull files and use rebase to make the repositories match.
+
+# 17) What’s a question you still have? What’s something you’re confused about?
+#I'm still not sure what a Jupyter notebook or lab is because we've never used them in class.
+
+# 18) Tell me a fun fact!
+#the ancient Minoan peoples used to jump/frontflip over bulls for sport (courtesy of my archeaology class)
+
+# 19) Print your favorite math expression you've learned in Python so far. 
+# (Hint: Use print() and add a comment explaining what it does.)
+x = 10%4
+print (x)
+#the percentage symbol prints the remainder of a division problem.
+
